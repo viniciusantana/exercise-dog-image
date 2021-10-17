0a34c5983d155876e650670fce5182c036647a2a
viniciusantana <viniciusantana@yahoo.com.br>
2021-10-17 18:17:10 -0300

    requisito 2

--- src/Componentes.js
+++ src/Componentes.js
@@ -21,10 +21,20 @@ class Componentes extends React.Component {
     const url = 'https://dog.ceo/api/breeds/image/random';
     const request = await fetch(url);
     const requestObject = await request.json();
-    this.setState((prevState) => ({
-      dogObject: [...prevState.dogObject ,requestObject.message],
-      loading: false,
-    }))
+    const imgUrl = requestObject.message;
+    if(imgUrl.includes('terrier')) {
+      this.setState({
+        loading: false,
+      })
+      alert('TERRIER - Clique em ADD-DOG e tente novamente')
+    }else {
+      this.setState((prevState) => ({
+        dogObject: [...prevState.dogObject ,imgUrl],
+        loading: false,
+      }));
+      // corta o início da url e o final ** Magic Numbers
+      alert(imgUrl.slice(30, imgUrl.indexOf('/', 30) - imgUrl.length));
+    }
   }
 
   render() {
