3e268279eafd6c39d23480f5663119a7fc4bbf2c
viniciusantana <viniciusantana@yahoo.com.br>
2021-10-18 13:45:16 -0300

    extras/raça na imagem

--- src/api.js
+++ src/api.js
@@ -3,37 +3,40 @@ import React from 'react';
 class Api extends React.Component {
   constructor() {
     super();
-    
     this.state = {
       dogObject: [],
       loading: true,
     };
+    this.dogApi = this.dogApi.bind(this);
   }
 
   componentDidMount() {
     this.dogApi();
   }
-  
-  dogApi = async () => {
+
+  async dogApi() {
     this.setState({
-      loading: true
-    })
-    const url = 'https://dog.ceo/api/breeds/image/random';
-    const request = await fetch(url);
+      loading: true,
+    });
+
+    const endpoint = 'https://dog.ceo/api/breeds/image/random';
+    const request = await fetch(endpoint);
     const requestObject = await request.json();
     const imgUrl = requestObject.message;
-    if(imgUrl.includes('terrier')) {
+    // corta o início da url e o final ** Magic Numbers
+    // const breed = imgUrl.slice(30, imgUrl.indexOf('/', 30) - imgUrl.length);
+
+    if (imgUrl.includes('terrier')) {
       this.setState({
         loading: false,
-      })
-      alert('TERRIER - Clique em ADD-DOG e tente novamente')
-    }else {
+      });
+      alert('TERRIER - Clique em ADD-DOG e tente novamente');
+    } else {
       this.setState((prevState) => ({
-        dogObject: [...prevState.dogObject ,imgUrl],
+        dogObject: [...prevState.dogObject, imgUrl],
         loading: false,
       }));
-      // corta o início da url e o final ** Magic Numbers
-      alert(imgUrl.slice(30, imgUrl.indexOf('/', 30) - imgUrl.length));
+      // alert(breed);
     }
   }
 
@@ -41,10 +44,15 @@ class Api extends React.Component {
     const { dogObject, loading } = this.state;
     return (
       <section>
-        <button type='button' onClick={ this.dogApi }>ADD-DOG</button>
+        <button type="button" onClick={ this.dogApi }>ADD-DOG</button>
         <section>
-          { dogObject.map((imgUrl) => (<img src={imgUrl} />)) }
-          {loading? 'Loading...': ''}
+          { dogObject.map((imgUrl, index) => (
+            <section key={ index } className="doggo">
+              <h2>{ imgUrl.slice(30, imgUrl.indexOf('/', 30) - imgUrl.length) }</h2>
+              <img src={ imgUrl } alt="DogImage" />
+            </section>
+          )) }
+          {loading ? 'Loading...' : ''}
         </section>
       </section>
     );
